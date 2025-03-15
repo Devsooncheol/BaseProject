595de4203e6a0a41605dd67eed2ed391586e79e9
sooncheol <scjang@i-sens.com>
2025-03-15 23:31:24 +0900

    first commit

---
+++ app/src/main/java/com/base/baseproject/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.base.baseproject
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.base.baseproject.util.TestUtil
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+    }
+}
