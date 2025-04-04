285eccc15c3b775aa92479c7c6843403af6613f4
sooncheol <scjang@i-sens.com>
2025-04-04 11:05:40 +0900

    1. Gradle 설정 변경

--- app/src/main/java/com/base/baseproject/MainActivity.kt
+++ app/src/main/java/com/base/baseproject/MainActivity.kt
@@ -2,7 +2,6 @@ package com.base.baseproject
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.base.baseproject.util.TestUtil
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
