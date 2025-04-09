7fbd6e80dcf28d0edc294caf5728e6cd7e6bbec1
sooncheol <scjang@i-sens.com>
2025-04-09 11:56:16 +0900

    [Jetpack] #Hilt viewModel

--- app/src/main/java/com/base/baseproject/ui/home/hilt/HiltViewModel.kt
+++ app/src/main/java/com/base/baseproject/ui/home/hilt/HiltViewModel.kt
@@ -3,10 +3,23 @@ package com.base.baseproject.ui.home.hilt
 import android.content.Intent
 import com.base.module.base.BaseViewModel
 import com.base.module.base.constants.BaseConstants
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.asSharedFlow
 import timber.log.Timber
+import javax.inject.Inject
 
-class HiltViewModel(): BaseViewModel() {
+@HiltViewModel
+class HiltViewModel @Inject constructor(
+    private val hiltFiledInject: HiltFiledInject,
+    private val hiltConstructorInjectATypeClass: HiltConstructorInjectATypeClass,
+    @TestModule.TestInterface2  private val testInterface2: TestInterface,
+) : BaseViewModel() {
+
+    fun test() {
+        hiltFiledInject.showTestLog()
+        hiltConstructorInjectATypeClass.showTestLog()
+        testInterface2.showString()
+    }
 
     override fun getIntentExtraNext(intent: Intent): Intent {
         Timber.d("getIntentExtraNext, tag : $TAG")
