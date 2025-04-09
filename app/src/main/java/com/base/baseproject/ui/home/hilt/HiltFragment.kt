27842b8a06e7358de09bd68139fe54f8a5583565
sooncheol <scjang@i-sens.com>
2025-04-09 11:56:34 +0900

    [Jetpack] #Hilt Fragment

--- app/src/main/java/com/base/baseproject/ui/home/hilt/HiltFragment.kt
+++ app/src/main/java/com/base/baseproject/ui/home/hilt/HiltFragment.kt
@@ -6,10 +6,23 @@ import androidx.fragment.app.viewModels
 import androidx.viewbinding.ViewBinding
 import com.base.baseproject.databinding.FragmentHiltBinding
 import com.base.module.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class HiltFragment : BaseFragment() {
 
     override val viewModel: HiltViewModel by viewModels()
+
+    @Inject lateinit var hiltFiledInject: HiltFiledInject
+
+    @Inject lateinit var hiltConstructorInjectATypeClass: HiltConstructorInjectATypeClass
+
+    @Inject lateinit var HiltConstructorInjectBTypeClass: HiltConstructorInjectBTypeClass
+
+    @Inject lateinit var hiltModuleInjectATypeClass: HiltModuleInjectATypeClass
+
 
     override fun getViewBinding(): ViewBinding {
         return FragmentHiltBinding.inflate(layoutInflater)
@@ -17,6 +30,14 @@ class HiltFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        hiltFiledInject.showTestLog()
+
+        hiltConstructorInjectATypeClass.showTestLog()
+        HiltConstructorInjectBTypeClass.showTestLog()
+
+        Timber.i(hiltModuleInjectATypeClass.showTestLog())
+        Timber.i(hiltModuleInjectATypeClass.showTestLog2())
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
