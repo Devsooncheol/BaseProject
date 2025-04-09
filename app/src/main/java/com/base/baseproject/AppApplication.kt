17b427e62ae3d48cd44b600e184bf2f63c22bd27
sooncheol <scjang@i-sens.com>
2025-04-09 09:18:07 +0900

    [InitialSetting] Timber 설정

--- app/src/main/java/com/base/baseproject/AppApplication.kt
+++ app/src/main/java/com/base/baseproject/AppApplication.kt
@@ -1,10 +1,14 @@
 package com.base.baseproject
 
 import android.app.Application
+import timber.log.Timber
 
 class AppApplication: Application() {
     override fun onCreate() {
         super.onCreate()
 
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }
