9f3d1327c72c069286b2e5f9189333abcd420c2d
sooncheol <scjang@i-sens.com>
2025-04-24 16:23:11 +0900

    [Jetpack] #DataStore #hilt Module 설정

--- app/src/main/java/com/base/baseproject/module/DataStoreModule.kt
+++ app/src/main/java/com/base/baseproject/module/DataStoreModule.kt
@@ -2,9 +2,12 @@ package com.base.baseproject.module
 
 import android.content.Context
 import androidx.datastore.core.DataStore
+import androidx.datastore.core.DataStoreFactory
 import androidx.datastore.dataStoreFile
 import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
+import com.base.baseproject.SettingsTest
+import com.base.baseproject.ui.home.datastroe.proto.serializer.SettingsTestSerializer
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -23,5 +26,21 @@ object DataStoreModule {
         return PreferenceDataStoreFactory.create {
             context.dataStoreFile("BaseProjectSample.preferences_pb")
         }
+    }
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ProtoDataStoreModule {
+
+    @Provides
+    @Singleton
+    fun provideProtoDataStore(
+        @ApplicationContext context: Context
+    ): DataStore<SettingsTest> {
+        return DataStoreFactory.create(
+            serializer = SettingsTestSerializer,
+            produceFile = { context.dataStoreFile("BaseProjectSample_Proto.preferences_pb") }
+        )
     }
 }
